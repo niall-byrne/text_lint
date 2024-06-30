c1e333fac1ed04259cacc5f41fde8de329a7e71b
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-06-30 13:26:35 -0400

    feat(LOOKUPS): count lookup

---
+++ text_lint/operations/lookups/to_count.py
@@ -0,0 +1,34 @@
+"""CountLookup class."""
+
+from typing import TYPE_CHECKING
+
+from text_lint.config import LOOKUP_TRANSFORMATION_PREFIX
+from text_lint.utilities.translations import _
+from .bases.lookup_encoder_base import LookupBase
+
+if TYPE_CHECKING:  # pragma: no cover
+  from text_lint.linter.states import LookupState
+
+YAML_EXAMPLE = """
+
+- name: count transformation lookup example
+  operation: validate_debug
+  saved:
+    - example.capture.to_count
+
+"""
+
+
+class CountLookup(LookupBase):
+  """CountLookup operation for ResultForest instances."""
+
+  hint = _("transform a save id into a count of values")
+  operation = LOOKUP_TRANSFORMATION_PREFIX + "count"
+
+  def apply(
+      self,
+      state: "LookupState",
+  ) -> None:
+    """Convert the current result set to a counted value."""
+
+    state.results = str(len(state.results))
