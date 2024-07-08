f77c32a20403644b358ec78846d6183dcfab5396
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-07-08 14:05:47 -0400

    feat(CLI): command and interface classes

---
+++ text_lint/cli/commands/documentation_command.py
@@ -0,0 +1,35 @@
+"""DocumentationCommand class."""
+
+from argparse import ArgumentParser, Namespace
+
+from text_lint.cli.commands.bases.command_base import CLICommandBase
+from text_lint.utilities.translations import _
+
+
+class DocumentationCommand(CLICommandBase):
+  """CLI command to document the available operations."""
+
+  command_help = _("document the available operations")
+  command_name = "doc"
+
+  arg_operations = _("operations")
+  arg_operations_help = _("the operation(s) to document")
+
+  def create_parser(self, command_parser: "ArgumentParser") -> None:
+    """Create an argument parser for this CLI command."""
+
+    command_parser.add_argument(
+        dest="operations",
+        help=self.arg_operations_help,
+        metavar=self.arg_operations,
+        nargs="+",
+        type=str,
+    )
+
+  def invoke(self, args: "Namespace") -> None:
+    """Invoke this CLI command."""
+
+    for operation in args.operations:
+      self.documentation.search(operation)
+
+    self.documentation.print()
