58b7336b76aca3800ea113f135087de53159d23b
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-07-11 12:44:21 -0400

    feat(VALIDATORS): equality validator

---
+++ text_lint/exceptions/validators.py
@@ -0,0 +1,52 @@
+"""Exceptions for the text_lint result validators."""
+from typing import TYPE_CHECKING
+
+from text_lint.utilities.translations import _, f
+from text_lint.utilities.whitespace import make_visible
+
+if TYPE_CHECKING:  # pragma: no cover
+  from text_lint.operations.validators.bases.validator_base import (
+      ValidationBase,
+  )
+
+
+class ValidationExceptionBase(ValueError):
+  """Base class for validation exceptions."""
+
+
+class ValidationFailure(ValidationExceptionBase):
+  """Raised when a validation operation fails."""
+
+  msg_fmt_validation_operation = _("  VALIDATION OPERATION: {0}")
+  msg_fmt_validation_schema_operation_name = _("  SCHEMA OPERATION NAME: {0}")
+  msg_fmt_validation_detail = _("  DETAIL: {0}")
+  msg_fmt_validation_hint = _("  HINT: '{0}'")
+
+  def __init__(
+      self,
+      description: str,
+      detail: str,
+      validator: "ValidationBase",
+  ) -> None:
+    message = description
+    message += f(
+        self.msg_fmt_validation_operation,
+        validator.__class__.__name__,
+        nl=1,
+    )
+    message += f(
+        self.msg_fmt_validation_schema_operation_name,
+        make_visible(validator.name),
+        nl=1,
+    )
+    message += f(
+        self.msg_fmt_validation_detail,
+        detail,
+    )
+    message += f(
+        self.msg_fmt_validation_hint,
+        validator.hint,
+        nl=1,
+    )
+
+    super().__init__(message)
