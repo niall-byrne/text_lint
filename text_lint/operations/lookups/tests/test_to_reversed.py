fed7d3c806a702230cf0b5d7379b1e05abffbaeb
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-07-13 14:58:00 -0400

    feat(LOOKUPS): reversed lookup

---
+++ text_lint/operations/lookups/tests/test_to_reversed.py
@@ -0,0 +1,89 @@
+"""Test the ReversedLookup class."""
+
+from typing import TYPE_CHECKING
+from unittest import mock
+
+from text_lint.__helpers__.lookups import result_reversing_test_cases
+from text_lint.__helpers__.operations import (
+    AliasOperationAttributes,
+    assert_operation_attributes,
+    assert_operation_inheritance,
+)
+from text_lint.__helpers__.translations import assert_is_translated
+from text_lint.config import LOOKUP_TRANSFORMATION_PREFIX
+from text_lint.operations.lookups.encoders.reversed import ReversedEncoder
+from ..bases.lookup_base import LookupBase
+from ..bases.lookup_encoder_base import LookupEncoderBase
+from ..to_reversed import ReversedLookup
+
+if TYPE_CHECKING:  # pragma: no-cover
+  from text_lint.results.forest import AliasLookupResult
+
+
+class TestReversedLookup:
+  """Test the ReversedLookup class."""
+
+  def test_initialize__defined__attributes(
+      self,
+      to_reversed_lookup_instance: ReversedLookup,
+      mocked_lookup_name: str,
+      mocked_requesting_operation_name: str,
+      mocked_result_set: mock.Mock,
+  ) -> None:
+    attributes: AliasOperationAttributes = {
+        "encoder_class": ReversedEncoder,
+        "hint": "reverse the order of the saved results",
+        "lookup_name": mocked_lookup_name,
+        "operation": LOOKUP_TRANSFORMATION_PREFIX + "reversed",
+        "requesting_operation_name": mocked_requesting_operation_name,
+        "result_set": mocked_result_set,
+    }
+
+    assert_operation_attributes(to_reversed_lookup_instance, attributes)
+
+  def test_initialize__translations(
+      self,
+      to_reversed_lookup_instance: ReversedLookup,
+  ) -> None:
+    assert_is_translated(to_reversed_lookup_instance.hint)
+
+  def test_initialize__inheritance(
+      self,
+      to_reversed_lookup_instance: ReversedLookup,
+  ) -> None:
+    assert_operation_inheritance(
+        to_reversed_lookup_instance,
+        bases=(
+            LookupBase,
+            LookupEncoderBase,
+            ReversedLookup,
+        ),
+    )
+
+  @result_reversing_test_cases
+  def test_apply__vary_forest_lookup_results__updates_forest_lookup_results(
+      self,
+      to_reversed_lookup_instance: ReversedLookup,
+      mocked_controller: mock.Mock,
+      result: "AliasLookupResult",
+      expected: "AliasLookupResult",
+  ) -> None:
+    mocked_controller.forest.lookup_results = result
+
+    to_reversed_lookup_instance.apply(mocked_controller)
+
+    for value_a, value_b in zip(
+        mocked_controller.forest.lookup_results,
+        expected,
+    ):
+      assert value_a == value_b
+
+  def test_apply__string_results__updates_forest_lookup_results(
+      self,
+      to_reversed_lookup_instance: ReversedLookup,
+      mocked_controller: mock.Mock,
+  ) -> None:
+    mocked_controller.forest.lookup_results = "aaccbb"
+    to_reversed_lookup_instance.apply(mocked_controller)
+
+    assert mocked_controller.forest.lookup_results == "bbccaa"
---
+++ text_lint/operations/lookups/to_reversed.py
@@ -0,0 +1,43 @@
+"""ReversedLookup class."""
+
+from typing import TYPE_CHECKING
+
+from text_lint.config import LOOKUP_TRANSFORMATION_PREFIX
+from text_lint.utilities.translations import _
+from .bases.lookup_encoder_base import LookupEncoderBase
+from .encoders.reversed import ReversedEncoder
+
+if TYPE_CHECKING:  # pragma: no cover
+  from text_lint.controller import Controller
+
+YAML_EXAMPLE = """
+
+- name: reversed transformation lookup example
+  operation: validate_debug
+  saved:
+    - example.capture.0.to_reversed
+
+"""
+
+
+class ReversedLookup(LookupEncoderBase):
+  """ReversedLookup operation for ResultForest instances."""
+
+  encoder_class = ReversedEncoder
+  hint = _("reverse the order of the saved results")
+  operation = LOOKUP_TRANSFORMATION_PREFIX + "reversed"
+
+  def apply(
+      self,
+      controller: "Controller",
+  ) -> None:
+    """Reverse the order of the current lookup results."""
+
+    if isinstance(controller.forest.lookup_results, str):
+      controller.forest.lookup_results = "".join(
+          reversed(list(controller.forest.lookup_results))
+      )
+    else:
+      controller.forest.lookup_results = self.encode(
+          controller.forest.lookup_results
+      )
