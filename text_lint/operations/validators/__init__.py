f09ef6cd183b175e42cb982969381d033c7d240e
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-07-13 14:52:38 -0400

    feat(VALIDATORS): combine validator

--- text_lint/operations/validators/__init__.py
+++ text_lint/operations/validators/__init__.py
@@ -3,10 +3,12 @@
 from typing import Dict, Type
 
 from .bases.validator_base import ValidatorBase
+from .validate_combine import ValidateCombine
 from .validate_debug import ValidateDebug
 from .validate_equal import ValidateEqual
 
 validator_registry: Dict[str, Type[ValidatorBase]] = {
+    ValidateCombine.operation: ValidateCombine,
     ValidateDebug.operation: ValidateDebug,
     ValidateEqual.operation: ValidateEqual,
 }
