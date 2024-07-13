48ad22ac7aa8d2fca8da3c3e2de6bd1e817eadc1
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-07-13 14:52:38 -0400

    feat(VALIDATORS): combine validator

--- text_lint/operations/validators/tests/conftest.py
+++ text_lint/operations/validators/tests/conftest.py
@@ -5,7 +5,12 @@ from typing import List
 from unittest import mock
 
 import pytest
-from .. import validate_debug, validate_equal
+from .. import validate_combine, validate_debug, validate_equal
+
+
+@pytest.fixture
+def mocked_combined_result_tree_name() -> str:
+  return "mocked_combined_result_name"
 
 
 @pytest.fixture
@@ -17,6 +22,19 @@ def mocked_controller() -> mock.Mock:
 @pytest.fixture
 def mocked_validator_name() -> str:
   return "mocked_validator_name"
+
+
+@pytest.fixture
+def validate_combine_instance(
+    mocked_combined_result_tree_name: str,
+    mocked_result_set_a: List[str],
+    mocked_validator_name: str,
+) -> validate_combine.ValidateCombine:
+  return validate_combine.ValidateCombine(
+      mocked_validator_name,
+      new_saved=mocked_combined_result_tree_name,
+      saved=mocked_result_set_a,
+  )
 
 
 @pytest.fixture
