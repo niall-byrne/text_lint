d1e051b7c6fa715c7209b77682da9f4462842b89
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-08-27 12:23:46 -0400

    feat(TRANSLATIONS): enable multilingual support

--- text_lint/utilities/tests/test_translations.py
+++ text_lint/utilities/tests/test_translations.py
@@ -1,5 +1,5 @@
 """Test the translations utilities."""
-
+import os
 from unittest import mock
 
 import text_lint.utilities.translations
@@ -40,3 +40,41 @@ class TestFString:
             1, key=2, nl=config.NEW_LINE
         )
     )
+
+
+class TestInitialize:
+  """Test the initialize function."""
+
+  @mock.patch.dict(
+      text_lint.utilities.translations.__name__ + ".os.environ",
+      {'LANG': 'ko_KR.UTF-8'},
+      clear=True
+  )
+  def test_lang_is_set__lang_remains_unchanged(self) -> None:
+    text_lint.utilities.translations.initialize()
+
+    assert os.getenv("LANG") == "ko_KR.UTF-8"
+
+  @mock.patch.dict(
+      text_lint.utilities.translations.__name__ + ".os.environ", {}, clear=True
+  )
+  def test_lang_not_set__default_lang_used(
+      self, mocked_locale: mock.Mock
+  ) -> None:
+    mocked_locale.getdefaultlocale.return_value = ['ko_KR.UTF-8']
+
+    text_lint.utilities.translations.initialize()
+
+    assert os.getenv("LANG") == mocked_locale.getdefaultlocale.return_value[0]
+
+  def test_any_lang__returns_initialized_translations(
+      self, mocked_gettext: mock.Mock
+  ) -> None:
+    return_value = text_lint.utilities.translations.initialize()
+
+    mocked_gettext.translation.assert_called_once_with(
+        "base",
+        os.path.join(os.path.dirname(text_lint.__file__), "locales"),
+        fallback=True,
+    )
+    assert return_value == mocked_gettext.translation.return_value
