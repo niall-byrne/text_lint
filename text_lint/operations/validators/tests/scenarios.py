12c71d45ead6b6fa44e027f41bdbf3b7a238948e
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-09-06 11:21:38 -0400

    feat(VALIDATORS): membership validator

--- text_lint/operations/validators/tests/scenarios.py
+++ text_lint/operations/validators/tests/scenarios.py
@@ -8,6 +8,9 @@ import pytest
 __all__ = (
     "scenario__comparison__lookup_results_equal",
     "scenario__comparison__lookup_results_not_equal",
+    "scenario__comparison__lookup_results_invalid_membership",
+    "scenario__comparison__lookup_results_membership",
+    "scenario__comparison__lookup_results_not_membership",
 )
 
 
@@ -30,4 +33,40 @@ def scenario__comparison__lookup_results_not_equal(
       "result_1",
       "result_0",
       "result_1",
+  )
+
+
+@pytest.fixture
+def scenario__comparison__lookup_results_invalid_membership(
+    mocked_state: mock.Mock
+) -> None:
+  mocked_state.lookup_expression.side_effect = (
+      "result_0",
+      ["result_1"],
+      "result_0",
+      ["result_1"],
+  )
+
+
+@pytest.fixture
+def scenario__comparison__lookup_results_membership(
+    mocked_state: mock.Mock
+) -> None:
+  mocked_state.lookup_expression.side_effect = (
+      ["result_0"],
+      "result_0",
+      ["result_1"],
+      "result_1",
+  )
+
+
+@pytest.fixture
+def scenario__comparison__lookup_results_not_membership(
+    mocked_state: mock.Mock
+) -> None:
+  mocked_state.lookup_expression.side_effect = (
+      ["result_0"],
+      "result_1",
+      ["result_1"],
+      "result_0",
   )
