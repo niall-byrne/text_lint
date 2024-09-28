f65c25d3a3bf417e9c848805b6edd5648281d4b4
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-09-28 18:54:39 -0400

    feat(CLI): deferred module loaders

--- text_lint/cli/commands/tests/conftest.py
+++ text_lint/cli/commands/tests/conftest.py
@@ -30,69 +30,70 @@ def mocked_args_list() -> mock.Mock:
 
 
 @pytest.fixture
-def mocked_linter() -> mock.Mock:
-  return mock.Mock()
-
-
-@pytest.fixture
-def mocked_linter_settings(
-    mocked_linter_settings_instances: List[mock.Mock]
-) -> mock.Mock:
-  return mock.Mock(side_effect=mocked_linter_settings_instances)
-
-
-@pytest.fixture
 def mocked_linter_settings_instances() -> List[mock.Mock]:
   return [mock.Mock(), mock.Mock(), mock.Mock()]
 
 
 @pytest.fixture
-def mocked_documentation() -> mock.Mock:
+def mocked_deferred_linter() -> mock.Mock:
+  return mock.Mock()
+
+
+@pytest.fixture
+def mocked_deferred_linter_settings(
+    mocked_linter_settings_instances: List[mock.Mock]
+) -> mock.Mock:
+  deferred_instance = mock.Mock(side_effect=mocked_linter_settings_instances)
+  return mock.Mock(return_value=deferred_instance)
+
+
+@pytest.fixture
+def mocked_deferred_documentation() -> mock.Mock:
   return mock.Mock()
 
 
 @pytest.fixture
 def check_command_instance(
-    mocked_linter: mock.Mock,
-    mocked_linter_settings: mock.Mock,
+    mocked_deferred_linter: mock.Mock,
+    mocked_deferred_linter_settings: mock.Mock,
     monkeypatch: pytest.MonkeyPatch,
 ) -> check_command.CheckCommand:
   monkeypatch.setattr(
       check_command,
-      "Linter",
-      mocked_linter,
+      "deferred_linter",
+      mocked_deferred_linter,
   )
   monkeypatch.setattr(
       check_command,
-      "LinterSettings",
-      mocked_linter_settings,
+      "deferred_linter_settings",
+      mocked_deferred_linter_settings,
   )
   return check_command.CheckCommand()
 
 
 @pytest.fixture
 def documentation_command_instance(
-    mocked_documentation: mock.Mock,
+    mocked_deferred_documentation: mock.Mock,
     monkeypatch: pytest.MonkeyPatch,
 ) -> documentation_command.DocumentationCommand:
   instance = documentation_command.DocumentationCommand()
   monkeypatch.setattr(
-      instance,
-      "documentation",
-      mocked_documentation,
+      documentation_command,
+      "deferred_operation_documentation",
+      mocked_deferred_documentation,
   )
   return instance
 
 
 @pytest.fixture
 def list_command_instance(
-    mocked_documentation: mock.Mock,
+    mocked_deferred_documentation: mock.Mock,
     monkeypatch: pytest.MonkeyPatch,
 ) -> list_command.ListCommand:
   instance = list_command.ListCommand()
   monkeypatch.setattr(
-      instance,
-      "documentation",
-      mocked_documentation,
+      list_command,
+      "deferred_operation_documentation",
+      mocked_deferred_documentation,
   )
   return instance
